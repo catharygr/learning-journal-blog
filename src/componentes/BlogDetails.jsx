746e4a43e845c4d94f1a0511de4c7c2c4803b8dc
catharygr <103951447+catharygr@users.noreply.github.com>
2023-05-16 19:01:21 +0100

    Trabajando con el componente MarkDown

--- src/componentes/BlogDetails.jsx
+++ src/componentes/BlogDetails.jsx
@@ -1,20 +1,24 @@
 import data from "../data";
 import { useParams } from "react-router-dom";
-
 import BlogMain from "./BlogMain";
+import { ReactMarkdown } from "react-markdown/lib/react-markdown";
 
 export default function BlogDetails() {
   const params = useParams();
   const blogs = data.find((blog) => blog.id === params.id);
   const fecha = new Date(blogs.date);
 
+  q;
+  const descriptionMD = blogs.description.replace(/\\n\\n/g, "\n\n");
+
   return (
     <div>
-      <div className="">
-        <p>{fecha.toLocaleDateString()}</p>
+      <div className="container-blog">
+        <p className="fecha-blog">{fecha.toLocaleDateString()}</p>
         <h2>{blogs.title}</h2>
-        <p>{blogs.description}</p>
-        <div className="img-aspect-ratio">
+        <ReactMarkdown>{descriptionMD}</ReactMarkdown>
+        <p className="blog-description">{blogs.description}</p>
+        <div className="img-blog">
           <img src={blogs.imgUrl2} alt={blogs.title} />
         </div>
         <p>{blogs.text}</p>
